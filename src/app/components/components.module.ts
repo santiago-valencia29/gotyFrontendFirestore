eabe89546223be7ade5774336f1c0e8b6cb7dc20
Santiago Valencia <edy-santiago@outlook.com>
2020-12-15 21:14:44 -0500

    configuración del grafico-responsive ngx-charts

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,8 +4,8 @@ import { RouterModule } from '@angular/router';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { GraficoBarraHorizontalComponent } from './grafico-barra-horizontal/grafico-barra-horizontal.component';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,9 @@ import { GraficoBarraHorizontalComponent } from './grafico-barra-horizontal/graf
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    BrowserAnimationsModule,
+    NgxChartsModule
   ]
 })
 export class ComponentsModule { }
--- src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grafico-barra-horizontal',
   templateUrl: './grafico-barra-horizontal.component.html',
-  styleUrls: ['./grafico-barra-horizontal.component.css']
+  styleUrls: ['./grafico-barra-horizontal.component.css'],
 })
-export class GraficoBarraHorizontalComponent implements OnInit {
+export class GraficoBarraHorizontalComponent {
+  results: any[] = [
+    {
+      name: 'Juego1',
+      value: 20,
+    },
+    {
+      name: 'Juego2',
+      value: 25,
+    },
+    {
+      name: 'Juego3',
+      value: 15,
+    },
+    {
+      name: 'Juego4',
+      value: 30,
+    },
+  ];
 
-  constructor() { }
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = true;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Juegos';
+  showYAxisLabel = true;
+  yAxisLabel = 'Votos';
 
-  ngOnInit(): void {
+  colorScheme = 'nightLights';
+
+  constructor() {}
+
+  onSelect(event: any) {
+    console.log(event);
   }
-
 }
