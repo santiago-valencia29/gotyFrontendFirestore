046d1d50a36ccbdf4dee6e882641e54684f50b34
Santiago Valencia <edy-santiago@outlook.com>
2020-12-15 21:45:34 -0500

    simulacion de grafica y eliminacion de intervalo

--- src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-grafico-barra-horizontal',
   templateUrl: './grafico-barra-horizontal.component.html',
   styleUrls: ['./grafico-barra-horizontal.component.css'],
 })
-export class GraficoBarraHorizontalComponent {
+export class GraficoBarraHorizontalComponent implements OnDestroy {
   results: any[] = [
     {
       name: 'Juego1',
@@ -37,9 +37,23 @@ export class GraficoBarraHorizontalComponent {
 
   colorScheme = 'nightLights';
 
-  constructor() {}
+  intervalo;
+
+  constructor() {
+    this.intervalo = setInterval(() => {
+      const newResults = [...this.results];
+      for (let i in newResults) {
+        newResults[i].value = Math.round(Math.random() * 500);
+      }
+      this.results = [...newResults];
+    }, 1500);
+  }
 
   onSelect(event: any) {
     console.log(event);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalo);
   }
 }
