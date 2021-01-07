ff288c183b0e78e801d5ab53278363122fc36eff
Santiago Valencia <edy-santiago@outlook.com>
2021-01-06 22:32:14 -0500

    install angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { ComponentsModule } from './components/components.module';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AppComponent } from './app.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { GotyComponent } from './pages/goty/goty.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent, InicioComponent, GotyComponent],
@@ -15,6 +18,8 @@ import { GotyComponent } from './pages/goty/goty.component';
     AppRoutingModule,
     ComponentsModule,
     HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
   url: 'https://us-central1-firestore-grafica-7c1da.cloudfunctions.net',
+  firebase: {
+    apiKey: 'AIzaSyCcWA79A-PXC8BeyXR635Oyk7W2sSwZ1X0',
+    authDomain: 'firestore-grafica-7c1da.firebaseapp.com',
+    projectId: 'firestore-grafica-7c1da',
+    storageBucket: 'firestore-grafica-7c1da.appspot.com',
+    messagingSenderId: '636314644335',
+    appId: '1:636314644335:web:0c5bf2dac01cf6580c029a',
+  },
 };
